217965b0a8ab6e6e14043408d44c543c78ef918a
Luis J. Rodriguez O. <ljavierrodriguez@gmail.com>
2025-12-26 13:28:39 -0300

    Fixing Puertos

--- app/schemas/puerto.py
+++ app/schemas/puerto.py
@@ -5,6 +5,7 @@ from typing import Optional
 class PuertoCreate(BaseModel):
     nombre: str = Field(..., description="Nombre del puerto")
     #codigo: Optional[str] = Field(None, description="Código del puerto")
+    id_ciudad: int = Field(..., description="ID de la ciudad")
     descripcion: Optional[str] = Field(None, description="Descripción del puerto")
     model_config = ConfigDict(json_schema_extra={"examples": [{"nombre": "Valparaíso", "codigo": "VL", "descripcion": "Puerto principal de Chile"}]})
 
@@ -13,6 +14,7 @@ class PuertoRead(BaseModel):
     id_puerto: int = Field(..., description="ID único del puerto")
     nombre: str = Field(..., description="Nombre del puerto")
     #codigo: Optional[str] = Field(default=None, description="Código del puerto")
+    id_ciudad: int = Field(..., description="ID de la ciudad")
     descripcion: Optional[str] = Field(default=None, description="Descripción del puerto")
     model_config = ConfigDict(from_attributes=True)
 
@@ -20,5 +22,6 @@ class PuertoRead(BaseModel):
 class PuertoUpdate(BaseModel):
     nombre: Optional[str] = Field(None, description="Nombre del puerto")
     #codigo: Optional[str] = Field(None, description="Código del puerto")
+    id_ciudad: int = Field(..., description="ID de la ciudad")
     descripcion: Optional[str] = Field(None, description="Descripción del puerto")
     model_config = ConfigDict()
